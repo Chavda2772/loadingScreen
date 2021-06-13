ecac7d7d063976e2f7ac6bebac30a2834473009b
Mahesh Chavda <66986475+Chavda2772@users.noreply.github.com>
2021-06-13 14:48:23 +0530

    remove js file

--- main.js
+++
@@ -1,8 +0,0 @@
-var loadingScreen = document.getElementById("loadingScreen");
-
-window.addEventListener("load", (event) => {
-  loadingScreen.classList.add("hide-loading");
-  setTimeout(() => {
-    loadingScreen.remove();
-  }, 1000);
-});
